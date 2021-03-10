bc2a8b245908e44fc8dc5c446944c6e9fba0abf3
thinhngo-x <duc-thinh.ngo@polytechnique.edu>
2021-03-10 16:40:43 +0100

    Removed  batchnorm

--- deep_qlearning.py
+++ deep_qlearning.py
@@ -48,7 +48,7 @@ class DQN(nn.Module):
         self.convo = nn.Sequential(
             nn.Conv2d(n_channels, 4, 3, stride=1, padding=1),
             nn.LeakyReLU(),
-            nn.BatchNorm2d(4),
+            # nn.BatchNorm2d(4),
             # nn.Conv2d(4, 8, 3, stride=1, padding=1),
             # nn.LeakyReLU(),
             # nn.BatchNorm2d(8),
@@ -60,7 +60,7 @@ class DQN(nn.Module):
         self.head = nn.Sequential(
             nn.Linear(n*n*4, n*n*2),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(n*n*2),
+            # nn.BatchNorm1d(n*n*2),
             nn.Linear(n*n*2, n*n)
         )
 
